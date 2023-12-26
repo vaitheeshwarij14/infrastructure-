79cea79f2724319c661c900743f60b4d4bfd819c
vaitheeshwarij14 <154435285+vaitheeshwarij14@users.noreply.github.com>
2023-12-26 08:00:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,15 +15,24 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = "ap-south-1a"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
+
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.private_subnet_cidr
-  availability_zone       = "ap-south-1b"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1b"
+  
+  tags = {
+    Name = "private-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
